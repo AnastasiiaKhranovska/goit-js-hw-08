13d791ce4dac149e11a3245a26816c75ec21f46a
AnastasiiaKhranovska <123419111+AnastasiiaKhranovska@users.noreply.github.com>
2023-06-16 15:43:12 -0500

    Dz

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate',  throttle( e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+    }, 1000)
+    );
+
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    console.error(error)
+});
+
+// var throttle = throttle(localStorage.setItem, 50000, {trailing: true});
+// player.on('timeupdate', throttle(function(data) {
+//     window.localStorage.setItem("videoplayer-current-time", data.seconds);
+// }, 250));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('input[name="message"]');
+const LocalStorageKey = 'feedback-form-state';
+
+form.addEventListener(
+    'input',
+    throttle(e => {
+        const objectToSave = {email: email, message: message.value};
+        localStorage.setItem(LocalStorageKey, JSON.stringify(objectToSave));
+    }, 500)
+);
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log({email: email, message: message.value});
+    form.requestFullscreen();
+    localStorage.removeItem(LocalStorageKey);
+});
+
+const load = key => {
+    try {
+        const serializedState = localStorege.getItem(key);
+        return serializedState === null ? undefined : JSON.parse(serializedState;)
+    } catch(error) {
+        console.error('Get state error: ', error.message);
+    }
+};
+
+const storageData = load(LocalStorageKey);
+if (storageDate) {
+    email.value = storage.email;
+    message.value = storage.message;
+}
